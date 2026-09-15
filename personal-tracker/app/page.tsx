21d243318f7506ad6d0d4f79325958dbcf22f330
joshthibault308-cyber <joshthibault308@gmail.com>
2026-09-15 14:14:30 -0400

    Made it so the home page scales with mobile devices. I also added the login page and starting to add the create page

--- personal-tracker/app/page.tsx
+++ personal-tracker/app/page.tsx
@@ -19,13 +19,8 @@ export default function Home() {
           >
             Create Entry
           </a>
-<<<<<<< HEAD
           <div className="flex gap-5 items-center">
             <Image src="/Arrow.png" alt="Arrow" width={32} height={32} className="rotate-180" />
-=======
-          <div className="flex gap-[10%] items-center">
-            <Image src="/Arrow.png" alt="Arrow" width={32} height={32} className="rotate-180 h-auto w-full" />
->>>>>>> 766fc32 (Fixed text color)
             <div className="flex text-[#000000]">
               1/2
             </div>
